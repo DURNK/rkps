df932b9727b380706d0f7fa88e035830d048e9f6
Denis <durnker@gmail.com>
2022-03-22 20:54:54 +0200

    reworked logic for game, added reset button

--- javascript.js
+++ javascript.js
@@ -9,69 +9,39 @@ function computerPlay () {
 
 
 function playRound (playerChoice) {
-    const computerChoice = computerPlay();
+    
     let rock= "rock"
     let paper= "paper"
     let scissors= "scissors"
-
-    if (playerChoice.toLowerCase() === rock.toLowerCase() ){
-        playerChoice = "rock" // foreces player choice to be lowecase (for some reason code above does not work)
-        console.log(`Player chose ${playerChoice}`);
+    playerChoice = playerChoice.toLowerCase();
+    console.log(`Player chose ${playerChoice}`);
     }
-        else if (playerChoice.toLowerCase() === paper.toLowerCase() ){
-            playerChoice = "paper"
-        console.log(`Player chose ${playerChoice}`)
-        }
-        else if(playerChoice.toLowerCase() === scissors.toLowerCase() ){
-            playerChoice = "scissors"
-            console.log(`Player chose ${playerChoice}`)
-        }
-        else {alert("You did not enter any of the correct options")}
 
+function compareWinner (playerChoice, computerChoice) {
 
- //ROCK
-    if (playerChoice === "rock" && computerChoice === "rock"  ) {
-      console.log(`Both players chose rock, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
+ 
+    if (playerChoice == computerChoice) {
+        console.log(`TIE! Both players chose ${playerChoice}`)
     }
-        else if (playerChoice === "rock" && computerChoice === "paper" ) {
-         computerScore += 1;
-         console.log(`Computer wins ${computerChoice} wrapps ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
+    else if ((playerChoice == "scissors" && computerChoice == "paper") ||
+             (playerChoice == "paper" && computerChoice == "rock") || 
+             (playerChoice == "rock" && computerChoice == "scissors"))
+             {
+                 console.log(`Player wins!! ${playerChoice} beats ${computerChoice}`);
 
-   }
-        else if (playerChoice === "rock" && computerChoice === "scissors" ) {
-        playerScore += 1;
-         console.log(`Player wins ${playerChoice} destroys ${computerChoice} | Score: Player ${playerScore} AI ${computerScore} `);
-   }
-
-//PAPER
-   else if (playerChoice === "paper" && computerChoice === "paper" ) {
-    console.log(`Both players chose ${playerChoice}, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
-   }
-   else if (playerChoice === "paper" && computerChoice === "rock" ) {
-    playerScore += 1;
-    console.log(`Player wins ${playerChoice} wrapps ${computerChoice} | Score: Player ${playerScore} AI ${computerScore} `);
-   }
-   else if (playerChoice === "paper" && computerChoice === "scissors" ) {
-    computerScore += 1;
-    console.log(`Computer wins ${computerChoice} cuts ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
-   }
-
-//Scissors
-else if (playerChoice === "scissors" && computerChoice === "scissors" ) {
-    console.log(`Both players chose ${playerChoice}, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
- }
- else if (playerChoice === "scissors" && computerChoice === "rock" ) {
-    computerScore += 1;
-    console.log(`Computer wins ${computerChoice} destroys ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
- }
- else if (playerChoice === "scissors" && computerChoice === "paper" ) {
-    playerScore += 1;
-    console.log(`Player wins ${playerChoice} cuts ${computerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
- }
-else {console.log("error", computerChoice, playerChoice, computerScore, playerScore)}
-
-
+                 playerScore+=1;
+                 console.log(playerScore);
+                 return(playerScore);
+             }
+    else {
+        console.log(`Computer wins with ${computerChoice}`);
+        computerScore+=1;
+        console.log(computerScore);
+        return(computerScore);
+    }
 }
+
+
 let playerScore = 0;
 let computerScore = 0;
 let playerChoice;
@@ -98,40 +68,98 @@ buttons.forEach((button) =>
         else if (e.target.className == "btn3") {
             playerChoice = "scissors";  
         }
+        else if (e.target.className == "btn4") {
+            button.disabled = false;
+            playerScore = 0;
+            computerScore = 0; 
+        }
         console.log(i);
         }
+
+
+
+    const computerChoice = computerPlay();
     playRound(playerChoice);
+    compareWinner(playerChoice, computerChoice)
+
     content.textContent = `Player Score is ${playerScore} || Computer Score is ${computerScore}`;
+
     if(playerScore==5)
     {
     content.textContent = `Player WINS! With a score of ${playerScore}`;
+    button.disabled = true
+
     }
     else if(computerScore==5) 
     {
     content.textContent = `Computer WINS! With a score of ${computerScore}`;
+    button.disabled = true
     }
     });
 });
-
-
-
-
-
-
-// const playerChoice = playerPlay();
-//   function game() {
-//       let playerPoints = 0;
-//       let computerPoints = 0;
-//      for (i=1; i<5; i++){
-//      playerPoints = playerPoints + playerScore;
-//      computerPoints = computerPoints + computerScore;
-//      console.log(playerPoints, playerScore, computerPoints, computerScore,);
-//      console.log(`Player Score is ${playerPoints} || Computer Score is ${computerPoints}`);
-//     };
-//  }
-
 
 const container = document.querySelector('#container');
 const content = document.createElement('div');
 content.classList.add('content');
 container.appendChild(content);
+
+
+
+
+
+
+// if (playerChoice.toLowerCase() === rock.toLowerCase() ){
+//     playerChoice = "rock" // foreces player choice to be lowecase (for some reason code above does not work)
+//     console.log(`Player chose ${playerChoice}`);
+// }
+//     else if (playerChoice.toLowerCase() === paper.toLowerCase() ){
+//         playerChoice = "paper"
+//     console.log(`Player chose ${playerChoice}`)
+//     }
+//     else if(playerChoice.toLowerCase() === scissors.toLowerCase() ){
+//         playerChoice = "scissors"
+//         console.log(`Player chose ${playerChoice}`)
+//     }
+//     else {alert("You did not enter any of the correct options")}
+
+
+// //ROCK
+// if (playerChoice === "rock" && computerChoice === "rock"  ) {
+//   console.log(`Both players chose rock, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
+// }
+//     else if (playerChoice === "rock" && computerChoice === "paper" ) {
+//      computerScore += 1;
+//      console.log(`Computer wins ${computerChoice} wrapps ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
+
+// }
+//     else if (playerChoice === "rock" && computerChoice === "scissors" ) {
+//     playerScore += 1;
+//      console.log(`Player wins ${playerChoice} destroys ${computerChoice} | Score: Player ${playerScore} AI ${computerScore} `);
+// }
+
+// //PAPER
+// else if (playerChoice === "paper" && computerChoice === "paper" ) {
+// console.log(`Both players chose ${playerChoice}, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
+// }
+// else if (playerChoice === "paper" && computerChoice === "rock" ) {
+// playerScore += 1;
+// console.log(`Player wins ${playerChoice} wrapps ${computerChoice} | Score: Player ${playerScore} AI ${computerScore} `);
+// }
+// else if (playerChoice === "paper" && computerChoice === "scissors" ) {
+// computerScore += 1;
+// console.log(`Computer wins ${computerChoice} cuts ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
+// }
+
+// //Scissors
+// else if (playerChoice === "scissors" && computerChoice === "scissors" ) {
+// console.log(`Both players chose ${playerChoice}, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
+// }
+// else if (playerChoice === "scissors" && computerChoice === "rock" ) {
+// computerScore += 1;
+// console.log(`Computer wins ${computerChoice} destroys ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
+// }
+// else if (playerChoice === "scissors" && computerChoice === "paper" ) {
+// playerScore += 1;
+// console.log(`Player wins ${playerChoice} cuts ${computerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
+// }
+// else {console.log("error", computerChoice, playerChoice, computerScore, playerScore)
