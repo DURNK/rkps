cce381c8d4ba9c20acb2a3cf58de26cf90a7e475
Denis <durnker@gmail.com>
2022-04-05 00:01:53 +0300

    made website fancy

--- javascript.js
+++ javascript.js
@@ -22,18 +22,22 @@ function compareWinner (playerChoice, computerChoice) {
  
     if (playerChoice == computerChoice) {
         console.log(`TIE! Both players chose ${playerChoice}`)
+        compchoice.textContent = `TIE! Both players chose ${playerChoice}` ;
+
     }
     else if ((playerChoice == "scissors" && computerChoice == "paper") ||
              (playerChoice == "paper" && computerChoice == "rock") || 
              (playerChoice == "rock" && computerChoice == "scissors"))
              {
-                 console.log(`Player wins!! ${playerChoice} beats ${computerChoice}`);
+                compchoice.textContent = `Computer chose ${computerChoice} || Player wins, ${playerChoice} beats ${computerChoice}` ;
+
 
                  playerScore+=1;
                  console.log(playerScore);
                  return(playerScore);
              }
     else {
+        compchoice.textContent = `Computer chose ${computerChoice} || Computer wins,${computerChoice} beats ${playerChoice}` ;
         console.log(`Computer wins with ${computerChoice}`);
         computerScore+=1;
         console.log(computerScore);
@@ -47,15 +51,14 @@ let computerScore = 0;
 let playerChoice;
 
 
-const buttons = document.querySelectorAll('button');
-console.log(buttons);
-buttons.forEach((button) => 
+const buttons = document.querySelectorAll('a');
+buttons.forEach((a) => 
 {
-    button.addEventListener('click', function(e) 
+    a.addEventListener('click', function(e) 
     {
        
 
-        for (i=1; i<2; i++)
+        for (i=1; i<10; i++)
         {
 
 
@@ -68,12 +71,6 @@ buttons.forEach((button) =>
         else if (e.target.className == "btn3") {
             playerChoice = "scissors";  
         }
-        else if (e.target.className == "btn4") {
-            button.disabled = false;
-            playerScore = 0;
-            computerScore = 0; 
-        }
-        console.log(i);
         }
 
 
@@ -87,16 +84,29 @@ buttons.forEach((button) =>
     if(playerScore==5)
     {
     content.textContent = `Player WINS! With a score of ${playerScore}`;
-    button.disabled = true
-
+    playerScore = 0;
+    computerScore = 0; 
     }
     else if(computerScore==5) 
     {
     content.textContent = `Computer WINS! With a score of ${computerScore}`;
-    button.disabled = true
+    playerScore = 0;
+    computerScore = 0; 
     }
+
+
     });
+    a.addEventListener("mouseover", function(){
+    document.getElementById("body").style.background="green"
+    })
 });
+
+
+
+
+
+    
+
 
 const container = document.querySelector('#container');
 const content = document.createElement('div');
@@ -104,62 +114,8 @@ content.classList.add('content');
 container.appendChild(content);
 
 
+const fiona = document.querySelector('#fiona');
+const compchoice = document.createElement('div');
+compchoice.classList.add('compchoice');
+fiona.appendChild(compchoice);
 
-
-
-
-// if (playerChoice.toLowerCase() === rock.toLowerCase() ){
-//     playerChoice = "rock" // foreces player choice to be lowecase (for some reason code above does not work)
-//     console.log(`Player chose ${playerChoice}`);
-// }
-//     else if (playerChoice.toLowerCase() === paper.toLowerCase() ){
-//         playerChoice = "paper"
-//     console.log(`Player chose ${playerChoice}`)
-//     }
-//     else if(playerChoice.toLowerCase() === scissors.toLowerCase() ){
-//         playerChoice = "scissors"
-//         console.log(`Player chose ${playerChoice}`)
-//     }
-//     else {alert("You did not enter any of the correct options")}
-
-
-// //ROCK
-// if (playerChoice === "rock" && computerChoice === "rock"  ) {
-//   console.log(`Both players chose rock, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
-// }
-//     else if (playerChoice === "rock" && computerChoice === "paper" ) {
-//      computerScore += 1;
-//      console.log(`Computer wins ${computerChoice} wrapps ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
-
-// }
-//     else if (playerChoice === "rock" && computerChoice === "scissors" ) {
-//     playerScore += 1;
-//      console.log(`Player wins ${playerChoice} destroys ${computerChoice} | Score: Player ${playerScore} AI ${computerScore} `);
-// }
-
-// //PAPER
-// else if (playerChoice === "paper" && computerChoice === "paper" ) {
-// console.log(`Both players chose ${playerChoice}, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
-// }
-// else if (playerChoice === "paper" && computerChoice === "rock" ) {
-// playerScore += 1;
-// console.log(`Player wins ${playerChoice} wrapps ${computerChoice} | Score: Player ${playerScore} AI ${computerScore} `);
-// }
-// else if (playerChoice === "paper" && computerChoice === "scissors" ) {
-// computerScore += 1;
-// console.log(`Computer wins ${computerChoice} cuts ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
-// }
-
-// //Scissors
-// else if (playerChoice === "scissors" && computerChoice === "scissors" ) {
-// console.log(`Both players chose ${playerChoice}, DRAW | Score: Player ${playerScore} AI ${computerScore} `);
-// }
-// else if (playerChoice === "scissors" && computerChoice === "rock" ) {
-// computerScore += 1;
-// console.log(`Computer wins ${computerChoice} destroys ${playerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
-// }
-// else if (playerChoice === "scissors" && computerChoice === "paper" ) {
-// playerScore += 1;
-// console.log(`Player wins ${playerChoice} cuts ${computerChoice} | Score: Player ${playerScore} AI ${computerScore}  `);
-// }
-// else {console.log("error", computerChoice, playerChoice, computerScore, playerScore)
